818ded9b1fd4a1adc94a1cb2ca2e0ba334eac3a5
Joshua Kaluski <joshua@kaluski.com>
2019-06-07 17:08:09 -0400

    Implemented slider to change colour levels.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import Palette from './Palette';
+import {generatePalette} from "./colourHelpers";
 import seedPalettes from './seedPalettes';
 
 class App extends Component {
   render() {
     return (
         <div className="App">
-          <Palette {...seedPalettes[0]}/>
+          <Palette palette={generatePalette(seedPalettes[0])}/>
         </div>
     );
   }
--- src/Palette.js
+++ src/Palette.js
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
 import ColourBox from './ColourBox';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css'
 import './Palette.css';
 
 class Palette extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {level: 500};
+        this.changeLevel = this.changeLevel.bind(this);
+    }
+    changeLevel(level) {
+        this.setState({level: level});
+    }
     render() {
-        const colourBoxes = this.props.colours.map(colour => (
-            <ColourBox background={colour.colour} name={colour.name} />
+        const {colours} = this.props.palette;
+        const {level} = this.state;
+        const colourBoxes = colours[level].map(colour => (
+            <ColourBox background={colour.hex} name={colour.name} />
         ));
         return (
             <div className="Palette">
+                <Slider
+                    defaultValue={level}
+                    min={100}
+                    max={900}
+                    step={100}
+                    onAfterChange={this.changeLevel}
+                />
                 {/*Navbar goes here*/}
                 <div className="Palette-colours">
                     {/*Colour boxes*/}
