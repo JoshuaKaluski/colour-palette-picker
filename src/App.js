3f2e912048c22edd03de15f1157b8db6c817c71b
Joshua Kaluski <joshua@kaluski.com>
2019-06-08 13:40:54 -0400

    Added basic routing.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
 import Palette from './Palette';
 import {generatePalette} from "./colourHelpers";
 import seedPalettes from './seedPalettes';
 
 class App extends Component {
+  //Method to return a palette based on its id
+  findPalette(id) {
+      return seedPalettes.find(palette => {
+          return palette.id === id;
+      })
+  }
+
   render() {
     return (
-        <div className="App">
-          <Palette palette={generatePalette(seedPalettes[0])}/>
-        </div>
+        <Switch>
+            <Route
+                exact path="/"
+                render={() => <h1>Palette list goes here!</h1>}
+            />
+            <Route
+                exact path="/palette/:id"
+                render={(routeProps) => (
+                  <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>
+                )}
+            />
+            {/*<div className="App">
+                <Palette palette={generatePalette(seedPalettes[0])}/>
+            </div>*/}
+        </Switch>
     );
   }
 }
