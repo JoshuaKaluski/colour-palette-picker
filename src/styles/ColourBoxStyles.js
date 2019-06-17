2af00581e0a33142c4d09d44a4d9ad45683084ec
Joshua Kaluski <joshua@kaluski.com>
2019-06-17 15:23:54 -0400

    Added responsive helper and styled ColourBox

--- src/styles/ColourBoxStyles.js
+++ src/styles/ColourBoxStyles.js
@@ -1,4 +1,5 @@
 import chroma from "chroma-js";
+import sizes from './sizes';
 
  export default {
   ColourBox: {
@@ -12,6 +13,18 @@ import chroma from "chroma-js";
     "&:hover button": {
       opacity: "1",
       transition: "0.5s"
+    },
+    [sizes.down("lg")]: {
+      width: "25%",
+      height: props => props.showingFullPalette ? "20%" : "10%",
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: props => props.showingFullPalette ? "10%" : "10%",
+    },
+    [sizes.down("xs")]: {
+      width: "100%",
+      height: props => props.showingFullPalette ? "5%" : "10%",
     }
   },
   copyText: {
