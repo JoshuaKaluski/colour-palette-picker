be72e2261fc99bfa67aea609b5931c58baf216f7
Joshua Kaluski <joshua@kaluski.com>
2019-06-17 13:38:24 -0400

    Style tweaks

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -4,7 +4,7 @@ import AppBar from "@material-ui/core/AppBar";
 import classNames from "classnames";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from '@material-ui/icons/Menu';
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos'
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import {Link} from "react-router-dom";
@@ -58,7 +58,7 @@ class PaletteFormNav extends Component {
               onClick={handleDrawerOpen}
               className={classNames(classes.menuButton, open && classes.hide)}
             >
-              <MenuIcon />
+              <AddToPhotosIcon />
             </IconButton>
             <Typography variant="h6" color="inherit" noWrap>
               Palette Designer
